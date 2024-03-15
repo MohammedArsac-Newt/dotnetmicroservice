39bbfbbe2af97799de2f491879408e84c95a5522
MohammedArsac-Newt <arsacm@newtglobalcorp.com>
2024-03-15 11:43:23 +0530

    dotnet add

---
+++ user_service_app/Models/Users.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace user_service_app.Models
+{
+    public class Users
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string email { get; set; }
+    }
+}
